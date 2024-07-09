78ccdcd15d3ab8804d786d7a6d9110c54acad6e0
NguyenHuuPhuoc2002 <nguyenhuuphuocpdl@gmail.com>
2024-07-09 11:56:34 +0700

    update_7-9

--- FirstWeb/Views/IStudentRepository.cs
+++ FirstWeb/Views/IStudentRepository.cs
@@ -3,7 +3,8 @@
     public interface IStudentRepository<T>
     {
         Task<IEnumerable<T>> GetAllAsync();
-        Task<IEnumerable<T>> GetSearchAsync(string maNganh);
+        Task<IEnumerable<T>> GetSearchMaNganhAsync(string maNganh);
+        Task<IEnumerable<T>> GetSearchNameAsync(string name);
         Task<T> GetByIdAsync(string id);
         Task AddAsync(T entity);
         Task UpdateAsync(T entity);
